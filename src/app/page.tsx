ab8765db1cf73a421d9655710682195c64345ebb
Alexander Fedotov <alexfedotov@myurbanjungle.com>
2024-03-01 19:54:59 +0000

    setup data querying via `react-query`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,30 @@
 "use client";
 
+import { useQuery } from "@tanstack/react-query";
+import { DateTime } from "luxon";
+import { useState } from "react";
 
+import { IntensityResponse } from "@/networking/requests";
 
+export type TimeRange = {
+  start: DateTime;
+  end: DateTime;
+};
 
 export default function Home() {
+  // By default, we want to show today's data
+  const [timeRange, setTimeRange] = useState<TimeRange>({
+    start: DateTime.now().startOf("day"),
+    end: DateTime.now().endOf("day"),
+  });
 
+  const { isPending, error, data } = useQuery<IntensityResponse>({
+    queryKey: ["carbon-intensity", timeRange],
+    queryFn: () =>
+      fetch(
+        `https://api.carbonintensity.org.uk/intensity/${timeRange.start.toISO()}/${timeRange.end.toISO()}`,
+      ).then((res) => res.json()),
+  });
 
   return (
     <main className="">
@@ -12,6 +32,11 @@ export default function Home() {
         <h1>Carbon App</h1>
       </div>
       <div className="p-8">
+        <h2>Time Range</h2>
+        <p>
+          {timeRange.start.toLocaleString(DateTime.DATETIME_MED)} -{" "}
+          {timeRange.end.toLocaleString(DateTime.DATETIME_MED)}
+        </p>
       </div>
     </main>
   );
