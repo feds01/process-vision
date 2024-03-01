ad9741a9f0537b572f2cb4e9ca2902ece42764ee
Alexander Fedotov <alexfedotov@myurbanjungle.com>
2024-03-01 19:55:19 +0000

    add `Loader` and `Error` components

---
+++ src/components/Error.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+type ErrorProps = {
+  /** The error that occurred when performing some operation. */
+  error: Error | null;
+};
+
+const ErrorWrapper = (props: ErrorProps) => {
+  return <div>Error</div>;
+};
+
+export default ErrorWrapper;
---
+++ src/components/Loader.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type LoaderProps = {};
+
+const Loader = (props: LoaderProps) => {
+  return <div>Loading</div>;
+};
+
+export default Loader;
